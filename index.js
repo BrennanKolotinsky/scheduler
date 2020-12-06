1f8b83d2fa074e4f5397efd8ced456cc08ae1dcd
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-12-05 19:22:30 -0500

    authentication system

--- index.js
+++ index.js
@@ -1,26 +1,64 @@
 const express = require('express');
 const path = require('path');
 const generatePassword = require('password-generator');
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-// Put all API endpoints under '/api'
-app.get('/api/passwords', (req, res) => {
-  const count = 5;
-
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map(i =>
-    generatePassword(12, false)
-  )
-
-  // Return them as json
-  res.json(passwords);
-
-  console.log(`Sent ${count} passwords`);
+app.get('/api/passwords', verifyToken, (req, res) => {
+	// authData is the user
+	jwt.verify(req.token, 'secretkey', (err, authData) => {
+	    if(err) {
+	      res.sendStatus(403);
+	    } else {
+	    	const count = 5;
+			const passwords = Array.from(Array(count).keys()).map(i =>
+			    generatePassword(12, false)
+			);
+			res.json(passwords);
+			console.log(`Sent ${count} passwords`);
+	    }
+	});
 });
+
+app.post('/api/login', verifyToken, (req, res) => {
+  // Mock user
+  const user = {
+    id: 1, 
+    username: 'brad',
+    email: 'brad@gmail.com'
+  }
+
+  // create the login token and send it back
+  jwt.sign({user}, 'secretkey', (err, token) => {
+    res.json({
+      token
+    });
+  });
+});
+
+// Verify Token -- middleware
+function verifyToken(req, res, next) {
+  // Get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // Check if bearer is undefined
+  if(typeof bearerHeader !== 'undefined') {
+    // Split at the space
+    const bearer = bearerHeader.split(' ');
+    // Get token from array
+    const bearerToken = bearer[1];
+    // Set the token
+    req.token = bearerToken;
+    // Next middleware
+    next();
+  } else {
+    // Forbidden
+    res.sendStatus(403);
+  }
+}
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
