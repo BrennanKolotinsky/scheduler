6507178e2b6e413f79d03f025d0f4feaebb9c352
Brennan Kolotinsky <brennankolotinsky@Brennans-MacBook-Pro.local>
2020-12-05 20:50:57 -0500

    better

--- server/index.js
+++ server/index.js
@@ -67,10 +67,9 @@ function verifyToken(req, res, next) {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 
 const port = process.env.PORT || 5000;
